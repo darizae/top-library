a155d727644fddd738893b388195cede32ca334a
darizae <danielarizae@gmail.com>
2023-03-23 19:24:39 +0100

    Set up ook object

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+// Making form visible
+
 const form = document.getElementById('form-space');
 
 const formIsVisible = () => form.classList.contains('invisible');
@@ -8,3 +10,17 @@ function makeFormVisible() {
 
 const addButton = document.getElementById('add-btn');
 addButton.addEventListener('click', makeFormVisible);
+
+// Book Object
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = function info() {
+  const readingStatus = this.read ? 'already read' : 'not read yet';
+  return `${this.title} by ${this.author}. ${this.pages} pages, ${readingStatus}.`;
+};
